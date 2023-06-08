4f18b4dc06abfb73b6f2fcc9ef7c617df81dd1ba
Matias SL <matiasdtc08@gmail.com>
2023-06-07 22:23:41 -0300

    tp

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
-import userRouter from './routers/user.router.js'
-import productRouter from './api/products/products.js'
+
+
 const app = express();
 app.use(express.json())
 
@@ -8,6 +8,6 @@ app.get('/',(request , response)=>{
     response.send('Hola Mundo');
 })
 
-app.use('/', petRouter)
+
 
 app.listen(8080, ()=>console.log('Server up'));
